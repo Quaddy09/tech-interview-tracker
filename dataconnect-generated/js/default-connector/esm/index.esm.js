c182bfb25ce38c46312961b857ab05c33a83263f
David Krisna <davidkrisna69@gmail.com>
2025-02-18 17:45:45 +0700

    3rd commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'tech-interview-tracker',
+  location: 'us-central1'
+};
